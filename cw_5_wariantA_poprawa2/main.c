2b0649587262c5b9d5cd869aaca4f1c38f7ccc48
Ola <>
2021-03-28 20:58:27 +0200

    cw_5_poprawa

---
+++ cw_5_wariantA_poprawa2/main.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int rekurencja(int n)
+{
+    if (n == 0)
+        return 2;
+    if (n == 1)
+        return 2;
+    if (n > 0 && n % 2 ==0)
+        return rekurencja((n/2)-1) + n;
+    if (n >0 && n % 2 ==1 )
+        return 2 * rekurencja(n - 1) - n;
+
+}
+
+int main()
+{
+    int n;
+    printf("Podaj wartosc liczby n: ");
+    scanf("%d",&n);
+
+    if(n >= 0)
+    {
+        printf("Wartosc wynosi: %d", rekurencja(n));
+    }
+    else
+        printf("Liczba n powinna byc wieksza od 0");
+
+    return 0;
+}
